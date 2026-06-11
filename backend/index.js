9ba0747f3b76da9823956254dbd70e5c1bd4a11f
Anshika Verma <anshiikavermaa@gmail.com>
2026-06-11 12:17:07 +0530

    writing server creation

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
+
+
 //start file --controller code ->konse command pe kha redirect krna hai
+const express=require('express');
+const  dotenv=require('dotenv');
+const cors=require('cors');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const http=require('http');
+const{Server}=require('socket.io')
+
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
@@ -9,7 +19,7 @@ const { pushRepo } = require("./controllers/push");
 const { pullRepo } = require("./controllers/pull");
 const { revertRepo } = require("./controllers/revert");
 
-
+dotenv.config();   
 
 yargs(hideBin(process.argv))
 .command('init', 'Initialize a new repository',{},initRepo) 
@@ -46,5 +56,31 @@ commitRepo(argv.message); //agrv k andr additional parameters jo cmd k sth user 
  
 
 function startServer(){
-    console.log("Server is running")
+   const app=express();
+   const port=process.env.PORT||3000;
+
+   app.use(bodyParser.json());
+   app.use(express.json());
+   const mongoURI=process.env.MONGODB_URL;
+   mongoose.connect(mongoURI).then(()=>{
+    console.log("MOngoDB connected! ")
+   }).catch((err)=>{
+    console.error("Unable to connect MongoDB : ",err)
+   });  //mongodb se connection stablish based on this url
+890
+
+app.use(cors({origin:'*'}));
+app.get("/",(req,res)=>{
+    res.send("Welcome !")
+});
+
+const httpServer=http.createServer(app);
+const io=new Server(httpServer,{cors:{
+    origin:"*",
+    methods:["GET","POST"]
+}},)
+
+io.on("connection",(socket)==>{
+    socket.on()
+})
 };
