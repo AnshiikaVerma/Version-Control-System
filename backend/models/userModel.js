f3e7737aab3532293c4d3bc54dbb1606682dedde
Anshika Verma <anshiikavermaa@gmail.com>
2026-06-11 13:42:00 +0530

    Models created

---
+++ backend/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose');
+const {Schema}=mongoose;
+
+const UserSchema=  new SChema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    repositories:[
+        {
+            default:[],
+            type:Schema.Types.ObjectId,
+            ref:"Repository"
+        }
+    ],
+    followeduser:[
+        {
+            default:[],
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ],
+     StarRepositories:[
+        {
+            default:[],
+            type:Schema.Types.ObjectId,
+            ref:"Repository"
+        }
+    ],
+})
+
+
+const User=mongoose.model("User",UserSchema);
+export default User;
