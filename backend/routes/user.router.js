862344352142e962b3e3a6a4cf6da4a6010a316d
Anshika Verma <anshiikavermaa@gmail.com>
2026-06-20 21:52:15 +0530

    profile route added

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -8,8 +8,12 @@ const userRouter=express.Router();
 userRouter.get("/allUsers",userController.getAllUsers);
 userRouter.post("/signup",userController.signup);
 userRouter.post("/login",userController.login);
-userRouter.get("/userProfile/:id",userController.getUserProfile);
 userRouter.put("/updateProfile/:id",userController.updateUserProfile);
 userRouter.delete("/deleteProfile/:id",userController.deleteUserProfile);
-
+userRouter.get("/userProfile/:id",userController.getUserProfile);
+userRouter.post("/user/starRepository",userController.starRepository);
+// userRouter.post(
+// "/user/unstarRepository",
+// userController.unstarRepository
+// );
 module.exports=userRouter; 
