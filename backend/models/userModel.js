a898c0da724c3ad18db028c53c3691aec97483dc
Anshika Verma <anshiikavermaa@gmail.com>
2026-06-20 21:53:29 +0530

    starRepo added in user Schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,7 +29,7 @@ const UserSchema=  new Schema({
             ref:"User"
         }
     ],
-     StarRepositories:[
+    starRepos:[
         {
             default:[],
             type:Schema.Types.ObjectId,
