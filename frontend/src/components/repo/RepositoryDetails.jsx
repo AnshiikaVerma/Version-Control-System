393078207b4f2d1e243140f7159c63f9cfeb6365
Anshika Verma <anshiikavermaa@gmail.com>
2026-06-22 18:00:27 +0530

    Issue form added

--- frontend/src/components/repo/RepositoryDetails.jsx
+++ frontend/src/components/repo/RepositoryDetails.jsx
@@ -7,6 +7,10 @@ const RepositoryDetails = () => {
   const { id } = useParams();
 
   const [repo, setRepo] = useState(null);
+  //issue states
+  const [issues, setIssues] = useState([]);
+const [title, setTitle] = useState("");
+const [description, setDescription] = useState("");
 
   useEffect(() => {
     const fetchRepo = async () => {
@@ -16,6 +20,13 @@ const RepositoryDetails = () => {
         );
 
         setRepo(res.data);
+          //fetching issues too
+          const issueRes = await axios.get(
+      `http://localhost:3002/issue/all/${id}`
+    );
+
+    setIssues(issueRes.data);
+
       } catch (err) {
         console.error(err);
       }
@@ -23,6 +34,32 @@ const RepositoryDetails = () => {
 
     fetchRepo();
   }, [id]);
+
+
+  const createIssue = async () => {
+  try {
+    await axios.post(
+      `http://localhost:3002/issue/create/${id}`,
+      {
+        title,
+        description,
+      }
+    );
+
+    const issueRes = await axios.get(
+      `http://localhost:3002/issue/all/${id}`
+    );
+
+    setIssues(issueRes.data);
+
+    setTitle("");
+    setDescription("");
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 
   if (!repo) {
     return <h2>Loading...</h2>;
@@ -52,8 +89,49 @@ const RepositoryDetails = () => {
         <h3>
           Issues :
           {" "}
-          {repo.issues?.length || 0}
+          {issues.length}
         </h3>
+
+         <hr/>
+
+  <h2>Issues ({issues.length})</h2>
+{
+  issues.map((issue) => (
+    <div key={issue._id}>
+      <h4>{issue.title}</h4>
+
+      <p>{issue.description}</p>
+
+      <p>Status: {issue.status}</p>
+
+      <hr />
+    </div>
+  ))
+}
+
+<h2>Create Issue</h2>
+
+<input
+  type="text"
+  placeholder="Issue Title"
+  value={title}
+  onChange={(e) => setTitle(e.target.value)}
+/>
+
+<br /><br />
+
+<textarea
+  placeholder="Issue Description"
+  value={description}
+  onChange={(e) => setDescription(e.target.value)}
+/>
+
+<br /><br />
+
+<button onClick={createIssue}>
+  Create Issue
+</button>
+
       </div>
     </>
   );
